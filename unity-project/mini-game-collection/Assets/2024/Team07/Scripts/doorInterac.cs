08f30f2e86fb1d3bd35db44c9a36f2c1c5513502
DevinsHELL <000907680@mohawkcollege.ca>
2024-11-14 11:44:55 -0500

    button and door added
    I added the button and door scripts they work somewhat but the timer is a bit off. also it was getting confused that both players had the same tag so i untagged player two for testing. So far it only works with one door

---
+++ unity-project/mini-game-collection/Assets/2024/Team07/Scripts/doorInterac.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public GameObject DoorC;
+    public static int doorTimer = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //countdown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonPress.inRange == true && Input.GetKeyDown(buttonPress.interactKey))
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false; // disables the renderer 
+            DoorC.GetComponent<BoxCollider>().enabled = false; //disable collider
+
+            //countdown();
+            InvokeRepeating("countdown", 0.0f, 5f);
+
+
+        }
+
+    }
+
+    void countdown()
+    {
+        //while (doorTimer == 5 ^ doorTimer > 0)
+        //{
+
+        doorTimer--;
+
+        Debug.Log("doorTimer -1");
+
+
+        if (doorTimer == 0 || Input.GetKeyUp(buttonPress.interactKey))
+        {
+            //doorTimer--;
+            // Debug.Log("doorTimer -1");
+
+            Debug.Log("=0");
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+            DoorC.GetComponent<BoxCollider>().enabled = true;
+            doorTimer = 5;
+        }
+        //}
+    }
+}
