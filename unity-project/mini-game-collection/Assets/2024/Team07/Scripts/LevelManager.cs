51ebca4f83a07b97ae0fdbd198e7c25555520148
cass cochrane <150483589+cass0023@users.noreply.github.com>
2024-11-20 21:23:56 -0500

    Add UI and WinConditions

--- unity-project/mini-game-collection/Assets/2024/Team07/Scripts/LevelManager.cs
+++ unity-project/mini-game-collection/Assets/2024/Team07/Scripts/LevelManager.cs
@@ -7,7 +7,10 @@ namespace MiniGameCollection.Games2024.Team07
     public class LevelManager : MonoBehaviour
     {
         public GameObject[] possibleLevel;
+        public P1Controller p1Controller;
         public MiniGameManager miniGameManager;
+        public GameObject winCondition;
+        public GameObject failCondition;
         private GameObject currentLevel;
         private int randomNum;
         // Start is called before the first frame update
@@ -20,6 +23,13 @@ namespace MiniGameCollection.Games2024.Team07
         // Update is called once per frame
         void Update()
         {
+            if(miniGameManager.IsTimerExpired && !p1Controller.hasFinished){
+                failCondition.SetActive(true);
+            }
+            if(p1Controller.hasFinished){
+               winCondition.SetActive(true);
+               miniGameManager.StopGame();
+            }
             if (currentLevel == null){
                 randomNum = Random.Range(0, possibleLevel.Length);
                 currentLevel = possibleLevel[randomNum];
